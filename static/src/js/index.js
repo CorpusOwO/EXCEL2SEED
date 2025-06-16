3ccde8c477825c2a27a9e213b5624465ecae725d
CorpusOwO <Corpuscivilis@protonmail.com>
2025-06-16 10:53:48 -0400

    feature: created convertComponent 1.0

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -2,6 +2,7 @@ function validateFile() {
     const input = document.getElementById("dropzone-file");
     const modal = document.getElementById("modalDialog");
     const file = input.files[0];
+    const convertContainer = document.getElementById('convertContainer')
 
     const allowed_ext = [".xls", ".xlsx", ".csv"];
     const filename = file.name;
@@ -10,6 +11,9 @@ function validateFile() {
     if (!allowed_ext.includes(file_ext)) {
         modal.classList.remove("hidden");
         input.value = "";
+    } else {
+        console.log(convertContainer)
+        convertContainer.classList.remove("hidden");
     }
 }
 
